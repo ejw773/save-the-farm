e565317fe1ad9c8e0057ccf9a52149c7104fece5
Elijah Wilcott <ejw773@gmail.com>
2022-03-22 22:37:26 -0400

    adds button styling

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -11,6 +11,19 @@ let secondOptions = document.getElementsByClassName('second-option');
 for (let i = 0; i < secondOptions.length; i++) {
   secondOptions[i].addEventListener('click', respondToClick);
 }
+let welcomeText = document.getElementById('welcome');
 
 let statusMenu = document.getElementById('status-button');
 statusMenu.addEventListener('click', respondToClick);
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', '../php/save_the_farm.php', true);
+xhr.onreadystatechange = function () {
+  if (xhr.readyState < 4) {
+    welcomeText.innerHTML = 'Loading...';
+  }
+  if (xhr.readyState === 4) {
+    welcomeText.innerHTML = xhr.responseText;
+  }
+};
+xhr.send();
---
+++ public_html/php/save_the_farm.php
@@ -0,0 +1,7 @@
+<?php
+
+$initial_message = "Ok, the game is about to begin. Will you be able to find the golden mouse statuette and save your great-aunt's farm? We wish you good luck!";
+
+$other_message = "Hello there. It's been a harrowing few weeks. First your toenail issue, and now Great-Aunt Natasha's emu farm is in danger of being reposessed! So here you are---after a brief stopover at the Mayo clinic---in uncle Boris's remote cabin in the heart of the Terror Woods. Family legend holds that a golden statue of immense value is hidden somewhere within these walls. Unfortunately, you have a lot of stuff to do today, so you can only devote about 25 minutes to finding the statue. I guess we'll see what happens.";
+
+echo $initial_message;
