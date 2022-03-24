72ef41f89bd47a70439fa7dca83cbda9651564b1
Elijah Wilcott <ejw773@gmail.com>
2022-03-23 21:25:38 -0400

    adds initial code

---
+++ public_html/php/printStatus.php
@@ -0,0 +1,7 @@
+<?php
+
+function printStatus()
+{
+  global $location, $wearing_glasses, $wearing_contacts, $has_mushrooms, $has_soup, $is_hungry, $needs_bathroom;
+  echo "You are in: $location\n";
+}
